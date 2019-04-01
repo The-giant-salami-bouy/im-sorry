2ec09e6a6a6080a81029f20d56f9cc2a46a28453
The-giant-salami-bouy <34245813+The-giant-salami-bouy@users.noreply.github.com>
2019-04-01 11:26:06 -0500

    Add files via upload

---
+++ ArrayTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(String[] args){
+        ArrayList<Integer> scores = new ArrayList();
+        Scanner keyboard  = new Scanner(System.in);
+
+        int addNum = 0;
+
+        int total = 0;
+        int avg = 0;
+        System.out.println("please enter 5 values, type 999 to exit");
+
+        String entry;
+        entry = JOptionPane.showInputDialog(null, "Enter a number ");
+        addNum = Integer.parseInt(entry);
+    }
+
+
+}
